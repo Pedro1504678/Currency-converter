bc2ba30735a3259019c49e19a3be728db4c47275
Pedro Bastos <pedro.bastos@LIS2B.com.br>
2021-09-21 12:44:14 -0300

    start

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:async/async.dart';
+import 'dart:convert';
+
+var url = Uri.parse("https://api.hgbrasil.com/finance?format=json&key=af1485d6");
+
+void main() async {
+
+  http.Response response = await http.get(url);
+  
+  
+  print(jsonDecode(response.body));
+
+  
+
+  runApp(
+    MaterialApp(
+      home: Container(),
+    ),
+  );
+}
